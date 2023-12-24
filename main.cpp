08eecd775957e9ccf6b15cd1bc533e48c4197ac2
Artur Kunicki <tektmap1@gmail.com>
2023-12-24 15:59:03 +0100

    FIX

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <fstream>
 #include <sstream>
 #include <vector>
 #include <map>
-#include <fstream>
+#include <algorithm>  // Dodane dla std::remove
+
 #include "fmt/core.h"
 
 struct Column {
@@ -20,64 +22,7 @@ class SimpleDatabase {
 private:
     std::map<std::string, Table> tables;
 
-public:
-    void createTable(const std::string& tableName, const std::vector<Column>& columns) {
-        Table table;
-        table.name = tableName;
-        table.columns = columns;
-        tables[tableName] = table;
-        fmt::print("Table {} created\n", tableName);
-    }
-
-    void dropTable(const std::string& tableName) {
-        auto it = tables.find(tableName);
-        if (it != tables.end()) {
-            tables.erase(it);
-            fmt::print("Table {} dropped\n", tableName);
-        } else {
-            fmt::print("Error: Table {} not found\n", tableName);
-        }
-    }
-
-    void addColumn(const std::string& tableName, const Column& column) {
-        auto it = tables.find(tableName);
-        if (it != tables.end()) {
-            it->second.columns.push_back(column);
-            fmt::print("Column {} added to table {}\n", column.name, tableName);
-        } else {
-            fmt::print("Error: Table {} not found\n", tableName);
-        }
-    }
-
-    void insertData(const std::string& tableName, const std::map<std::string, std::string>& data) {
-        auto it = tables.find(tableName);
-        if (it != tables.end()) {
-            it->second.rows.push_back(data);
-            fmt::print("Data inserted into table {}\n", tableName);
-        } else {
-            fmt::print("Error: Table {} not found\n", tableName);
-        }
-    }
-
-    void queryData(const std::string& tableName, const std::string& condition) {
-        auto it = tables.find(tableName);
-        if (it != tables.end()) {
-            fmt::print("Query results for table {} with condition '{}':\n", tableName, condition);
-            for (const auto& row : it->second.rows) {
-                bool meetsCondition = true;
-                if (meetsCondition) {
-                    for (const auto& entry : row) {
-                        fmt::print("{}: {}, ", entry.first, entry.second);
-                    }
-                    fmt::print("\n");
-                }
-            }
-        } else {
-            fmt::print("Error: Table {} not found\n", tableName);
-        }
-    }
-
-    void saveToBackup(const std::string& filename) {
+    void saveToFile(const std::string& filename) {
         std::ofstream file(filename);
         if (file.is_open()) {
             for (const auto& entry : tables) {
@@ -88,12 +33,7 @@ public:
                 for (const auto& row : entry.second.rows) {
                     file << "  ";
                     for (const auto& column : entry.second.columns) {
-                        auto valueIt = row.find(column.name);
-                        if (valueIt != row.end()) {
-                            file << fmt::format("{}: {}, ", column.name, valueIt->second);
-                        } else {
-                            file << fmt::format("{}: [Not Found], ", column.name);
-                        }
+                        file << fmt::format("{}: {}, ", column.name, row.at(column.name));
                     }
                     file << "\n";
                 }
@@ -105,48 +45,88 @@ public:
         }
     }
 
-    void loadFromBackup(const std::string& filename) {
-        std::ifstream file(filename);
-        if (file.is_open()) {
-            std::string line;
-            Table currentTable;
-            while (std::getline(file, line)) {
-                std::istringstream iss(line);
-                std::string token;
-                iss >> token;
-                if (token == "Table:") {
-                    if (!currentTable.name.empty()) {
-                        tables[currentTable.name] = currentTable;
-                    }
-                    currentTable.name.clear();
-                    currentTable.columns.clear();
-                    currentTable.rows.clear();
-                    iss >> currentTable.name;
-                } else if (!currentTable.name.empty() && token.back() == ':') {
-                    Column column;
-                    column.name = token.substr(0, token.size() - 1);
-                    iss >> token;
-                    column.type = token;
-                    currentTable.columns.push_back(column);
-                } else if (!currentTable.name.empty() && token == "row") {
-                    std::map<std::string, std::string> row;
-                    while (iss >> token && token.back() == ',') {
-                        token.pop_back();
-                        std::string key = token.substr(0, token.find(':'));
-                        std::string value = token.substr(token.find(':') + 1);
-                        row[key] = value;
-                    }
-                    currentTable.rows.push_back(row);
+public:
+    void createTable(const std::string& tableName, const std::vector<Column>& columns) {
+        Table table;
+        table.name = tableName;
+        table.columns = columns;
+        tables[tableName] = table;
+        fmt::print("Table {} created\n", tableName);
+    }
+
+    void insertData(const std::string& tableName, const std::map<std::string, std::string>& data) {
+        auto it = tables.find(tableName);
+        if (it != tables.end()) {
+            std::map<std::string, std::string> newRow;
+            for (const auto& column : it->second.columns) {
+                newRow[column.name] = "";
+            }
+
+            for (const auto& entry : data) {
+                auto columnIt = newRow.find(entry.first);
+                if (columnIt != newRow.end()) {
+                    columnIt->second = entry.second;
+                } else {
+                    fmt::print("Error: Column {} not found in table {}\n", entry.first, tableName);
+                    return;
                 }
             }
-            if (!currentTable.name.empty()) {
-                tables[currentTable.name] = currentTable;
-            }
-            file.close();
-            fmt::print("Database loaded from {}\n", filename);
+
+            it->second.rows.push_back(newRow);
+            fmt::print("Data inserted into table {}\n", tableName);
         } else {
-            fmt::print("Error: Unable to open file for loading\n");
+            fmt::print("Error: Table {} not found\n", tableName);
         }
+    }
+
+    void updateData(const std::string& tableName, const std::map<std::string, std::string>& setValues, const std::string& conditionColumn, const std::string& conditionValue) {
+        auto it = tables.find(tableName);
+        if (it != tables.end()) {
+            for (auto& row : it->second.rows) {
+                // Sprawdź warunek
+                auto conditionIt = row.find(conditionColumn);
+                if (conditionIt != row.end() && conditionIt->second == conditionValue) {
+                    // Aktualizuj wartości
+                    for (const auto& entry : setValues) {
+                        auto columnIt = row.find(entry.first);
+                        if (columnIt != row.end()) {
+                            columnIt->second = entry.second;
+                        } else {
+                            fmt::print("Error: Column {} not found in table {}\n", entry.first, tableName);
+                            return;
+                        }
+                    }
+                    fmt::print("Data updated in table {}\n", tableName);
+                    return;
+                }
+            }
+            fmt::print("Error: No rows found in table {} with condition\n", tableName);
+        } else {
+            fmt::print("Error: Table {} not found\n", tableName);
+        }
+    }
+
+    void deleteData(const std::string& tableName, const std::string& conditionColumn, const std::string& conditionValue) {
+        auto it = tables.find(tableName);
+        if (it != tables.end()) {
+            it->second.rows.erase(
+                    std::remove_if(
+                            it->second.rows.begin(),
+                            it->second.rows.end(),
+                            [&](const std::map<std::string, std::string>& row) {
+                                auto conditionIt = row.find(conditionColumn);
+                                return conditionIt != row.end() && conditionIt->second == conditionValue;
+                            }),
+                    it->second.rows.end());
+
+            fmt::print("Data deleted from table {}\n", tableName);
+        } else {
+            fmt::print("Error: Table {} not found\n", tableName);
+        }
+    }
+
+    void saveToBackup(const std::string& filename) {
+        saveToFile(filename);
     }
 };
 
@@ -154,63 +134,80 @@ int main() {
     SimpleDatabase database;
 
     std::string command;
-    std::cout << "Enter commands (type 'exit' to quit):\n";
-
     while (true) {
         std::cout << "> ";
         std::getline(std::cin, command);
 
-        if (command == "exit") {
-            break;
-        }
-
         std::istringstream iss(command);
-        std::string operation;
-        iss >> operation;
+        std::string cmd;
+        iss >> cmd;
 
-        if (operation == "createTable") {
+        if (cmd == "createTable") {
             std::string tableName;
             iss >> tableName;
 
             std::vector<Column> columns;
-            std::string columnName, columnType;
-            while (iss >> columnName >> columnType) {
-                columns.push_back({columnName, columnType});
+            std::string colName, colType;
+            while (iss >> colName >> colType) {
+                columns.push_back({colName, colType});
             }
 
             database.createTable(tableName, columns);
-        } else if (operation == "dropTable") {
+        } else if (cmd == "insert") {
             std::string tableName;
             iss >> tableName;
-            database.dropTable(tableName);
-        } else if (operation == "addColumn") {
-            std::string tableName, columnName, columnType;
-            iss >> tableName >> columnName >> columnType;
-            database.addColumn(tableName, {columnName, columnType});
-        } else if (operation == "insert") {
-            std::string tableName, columnValuePair;
-            iss >> tableName >> columnValuePair;
 
             std::map<std::string, std::string> data;
-            std::istringstream dataStream(columnValuePair);
-            std::string column, value;
-            while (std::getline(dataStream, column, ':') && std::getline(dataStream, value, ' ')) {
-                data[column] = value;
+            std::string colInfo;
+            while (iss >> colInfo) {
+                size_t colonPos = colInfo.find(':');
+                if (colonPos != std::string::npos) {
+                    std::string colName = colInfo.substr(0, colonPos);
+                    std::string colValue = colInfo.substr(colonPos + 1);
+                    data[colName] = colValue;
+                } else {
+                    fmt::print("Error: Invalid column format in command\n");
+                    break;
+                }
             }
 
             database.insertData(tableName, data);
-        } else if (operation == "query") {
-            std::string tableName, condition;
-            iss >> tableName >> condition;
-            database.queryData(tableName, condition);
-        } else if (operation == "save") {
+        } else if (cmd == "update") {
+            std::string tableName;
+            iss >> tableName;
+
+            std::map<std::string, std::string> setValues;
+            std::string colInfo;
+            while (iss >> colInfo) {
+                size_t equalPos = colInfo.find('=');
+                if (equalPos != std::string::npos) {
+                    std::string colName = colInfo.substr(0, equalPos);
+                    std::string colValue = colInfo.substr(equalPos + 1);
+                    setValues[colName] = colValue;
+                } else {
+                    fmt::print("Error: Invalid set values format in command\n");
+                    break;
+                }
+            }
+
+            std::string conditionColumn, conditionValue;
+            iss >> conditionColumn >> conditionValue;
+
+            database.updateData(tableName, setValues, conditionColumn, conditionValue);
+        } else if (cmd == "delete") {
+            std::string tableName;
+            iss >> tableName;
+
+            std::string conditionColumn, conditionValue;
+            iss >> conditionColumn >> conditionValue;
+
+            database.deleteData(tableName, conditionColumn, conditionValue);
+        } else if (cmd == "save") {
             std::string filename;
             iss >> filename;
             database.saveToBackup(filename);
-        } else if (operation == "load") {
-            std::string filename;
-            iss >> filename;
-            database.loadFromBackup(filename);
+        } else if (cmd == "exit") {
+            break;
         } else {
             std::cout << "Unknown command. Try again.\n";
         }
@@ -218,3 +215,15 @@ int main() {
 
     return 0;
 }
+
+
+/*
+
+createTable Employees ID int Name string Salary double Department string
+        insert Employees ID:1 Name:John Salary:50000 Department:HR
+save a.txt
+ delete Employees where Department=IT
+
+exit
+
+*/
